6e67b21c46dba7bc6842b97e2c7afacf2320e1b6
maximka300 <klimashevskiymaxim2003@gmail.com>
2025-01-12 13:13:17 +0300

    lavender : Welcom to AlphaDroid!

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -8,11 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit some common AlphaDroid stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+ALPHA_MAINTAINER := SenseiDeveloper
 
 PRODUCT_NAME := lineage_lavender
 PRODUCT_BRAND := Xiaomi
