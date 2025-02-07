130b9b1f404954f4e69320a460638b03b1e4ab15
maximka300 <klimashevskiy2003@gmail.com>
2025-02-07 21:50:14 +0300

    lavender: overlay: Convert overlays to RRO

--- device.mk
+++ device.mk
@@ -304,15 +304,18 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+# RRO-Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    SettingsOverlay \
-    NoCutoutOverlay \
-    NotchBarKiller
+    LavenderCarrierConfigOverlay \
+    LavenderFrameworksOverlay \
+    LavenderLineageSdkOverlay \
+    LavenderNotchBarKillerOverlay \
+    LavenderSettingsOverlay \
+    LavenderSettingsProviderOverlay \
+    LavenderSystemUIOverlay \
+    LavenderTelephonyOverlay \
+    LavenderWifiOverlay
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
